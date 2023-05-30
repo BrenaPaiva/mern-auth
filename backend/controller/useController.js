a77f0bcd233895f18b8eca6cefbb585bb3f7a8ee
Brena <bpaiva@uniaoquimica.com.br>
2023-05-30 09:56:42 -0300

    gerando token com bcrypt

--- backend/controller/useController.js
+++ backend/controller/useController.js
@@ -1,4 +1,5 @@
-import asyncHandler from 'express-async-handler'
+import asyncHandler from 'express-async-handler';
+import User from '../model/userModel.js';
 
 // @desc Auth user/set token
 //route POST/api/users/auth
@@ -16,8 +17,31 @@ const authUser = asyncHandler(async (req, res) => {
 //@access Public 
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
 
-  res.status(200).json({ message: 'Register User' })
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('Usuário já existe')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400);
+    throw new Error('Usuário inválido');
+  }
 })
 // @desc Logout user
 //route POST/api/users/logout
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 import useRoutes from './routes/useRoutes.js'
+connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/users', useRoutes);
 app.get('/', (req, res) => res.send(`Servidor iniciado`));
