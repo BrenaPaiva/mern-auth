fbc77db594719e0c09dbcb3403db9e62b8618a60
Brena <bpaiva@uniaoquimica.com.br>
2023-05-30 11:54:30 -0300

    add token JWT

--- backend/controller/useController.js
+++ backend/controller/useController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
 import User from '../model/userModel.js';
 
 // @desc Auth user/set token
@@ -6,10 +7,23 @@ import User from '../model/userModel.js';
 //@access Public 
 
 const authUser = asyncHandler(async (req, res) => {
-  // res.status(401);
-  // throw new Error("Algo se esgotou")
 
-  res.status(200).json({ message: 'Usuário Auth' })
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(401);
+    throw new Error('email inválido ou senha incorreta');
+  }
+
 })
 
 // @desc Register a new user
@@ -33,6 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
   })
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
